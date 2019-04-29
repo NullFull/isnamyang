675363fc88378abab4c14b0d486fd8beb26735f4
lexifdev <sl@lxf.kr>
2019-04-30 04:03:08 +0900

    backend를 django로

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,3 @@
+from isnamyang.wsgi import application
 
-
-def app(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ['hello'.encode('utf8')]
-
+app = application
