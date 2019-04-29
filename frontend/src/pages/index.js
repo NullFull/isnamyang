82b5677ec7e6919ebb89f11debe2a949ff5f77f2
lexifdev <sl@lxf.kr>
2019-04-30 03:25:42 +0900

    frontend stub 추가

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+
+export default Index
+
