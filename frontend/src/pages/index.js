da38e6b02dadade3cee723209b91dd2ff013b605
lexifdev <sl@lxf.kr>
2019-05-20 21:32:39 +0900

    barcode 인식해서 보여줍니다.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import Quagga from 'quagga'
 
 
 class Index extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            detected: []
+        }
+    }
+
+    _onDetect = data => {
+        let appended = this.state.detected.concat(data);
+        this.setState({
+            detected: appended
+        })
+    }
+
+    componentDidMount() {
+        Quagga.init({
+            inputStream : {
+                name : "Live",
+                type : "LiveStream",
+                constraints: {
+                    width: 640,
+                    height: 640
+                },
+                target: document.querySelector('#interactive')
+            },
+            decoder : {
+                readers : ["ean_reader"]
+            },
+            locate: true
+        }, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            Quagga.start();
+        });
+        Quagga.onDetected(this._onDetect)
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                <h1>Is Namyang?</h1>
+                <div>
+                    <ul>
+                    {this.state.detected.map(data => <li>
+                        {data.codeResult.code}
+                    </li>)}
+                    </ul>
+                </div>
+                <div>
+                    <div id="interactive" className="viewport"/>
+                </div>
+            </div>
         )
     }
 }
