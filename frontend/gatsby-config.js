dd3c33ae6ba3e41558401d9711d911515118e66a
lexifdev <sl@lxf.kr>
2019-09-09 17:58:58 +0900

    add manifesto for PWA

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -7,5 +7,16 @@ module.exports = {
         trackingId: `UA-143097345-1`,
       },
     },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `남양유없`,
+        short_name: `남양유없`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+      },
+    },
   ]
 }
