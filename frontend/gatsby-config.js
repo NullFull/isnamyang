0c6ecfc7f7cefdd736cc78365f9f4eabd82a4ca4
lexifdev <sl@lxf.kr>
2019-09-24 12:18:31 +0900

    fix typo

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    sitemetadata: {
-        title: "남양유없",
-        description: "바코드만 찍으면 남양의 손길이 닿은 제품인지 알 수 있는 남양유없입니다!",
-        url: "https://isnamyang.nullfull.kr",
-        image: "/isnamyang-logo.png"
+    siteMetadata: {
+        title: '남양유없',
+        description: '바코드만 찍으면 남양의 손길이 닿은 제품인지 알 수 있는 남양유없입니다!',
+        url: 'https://isnamyang.nullfull.kr',
+        image: '/isnamyang-logo.png'
     },
     plugins: [
         'gatsby-plugin-react-helmet',
